3825a031d4faaf57d42782ac680447fd59e6f9c6
kyori19 <kyori.dis@gmail.com>
2019-11-22 00:26:04 +0900

    Not to create backup file

--- install.sh
+++ install.sh
@@ -6,12 +6,12 @@ set -e
 do_install() {
   echo "Starting custom motd installation"
   echo "Disable motd-news"
-  sudo sed -ie 's/ENABLED=1/ENABLED=0/g' /etc/default/motd-news
+  sudo sed -i -e 's/ENABLED=1/ENABLED=0/g' /etc/default/motd-news
   echo "Disable other annoying messages"
   sudo chmod -x /etc/update-motd.d/10-help-text /etc/update-motd.d/80-livepatch /etc/update-motd.d/91-release-upgrade
   echo "Setup server infomation"
   sudo curl -fsSL https://motd.accelf.net/85-server-info.sh -o /etc/update-motd.d/85-server-info
-  sudo sed -ie "s/\$HOSTNAME/$HOSTNAME/g" /etc/update-motd.d/85-server-info
+  sudo sed -i -e "s/\$HOSTNAME/$HOSTNAME/g" /etc/update-motd.d/85-server-info
   sudo chmod +x /etc/update-motd.d/85-server-info
   echo "Finally edit server infomation by yourself"
   sudo vi /etc/update-motd.d/85-server-info
