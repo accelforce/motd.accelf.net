1b13c63c304e3a19776395cf9a00d852bcb18be5
kyori19 <kyori.dis@gmail.com>
2020-04-29 11:18:48 +0900

    Check if the file exists

--- install.sh
+++ install.sh
@@ -8,7 +8,15 @@ do_install() {
   echo "Disable motd-news"
   sudo sed -i -e 's/ENABLED=1/ENABLED=0/g' /etc/default/motd-news
   echo "Disable other annoying messages"
-  sudo chmod -x /etc/update-motd.d/10-help-text /etc/update-motd.d/80-livepatch /etc/update-motd.d/91-release-upgrade
+  if [ -e /etc/update-motd.d/10-help-text ]; then
+    sudo chmod -x /etc/update-motd.d/10-help-text
+  fi
+  if [ -e /etc/update-motd.d/80-livepatch ]; then
+    sudo chmod -x /etc/update-motd.d/80-livepatch
+  fi
+  if [ -e /etc/update-motd.d/91-release-upgrade ]; then
+    sudo chmod -x /etc/update-motd.d/91-release-upgrade
+  fi
   echo "Setup server infomation"
   sudo curl -fsSL https://motd.accelf.net/85-server-info.sh -o /etc/update-motd.d/85-server-info
   sudo sed -i -e "s/\$HOSTNAME/$HOSTNAME/g" /etc/update-motd.d/85-server-info
